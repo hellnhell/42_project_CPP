436680e66422c4058fec849aedcbba0f3867ace4
Elena Martin fernandez <emartin-@c1r9s2.42madrid.com>
2021-05-19 14:23:55 +0200

    brains

---
+++ Module_01/ex_04/ex_04.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex_04.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emartin- <emartin-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/19 14:00:38 by emartin-          #+#    #+#             */
+/*   Updated: 2021/05/19 14:08:12 by emartin-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *p = &str;
+	std::string &r = str;
+	
+	std::cout << "This is the pointer: " << *p << std::endl;
+	std::cout << "This is the reference: " << r << std::endl;
+
+}
