2ef56d152e07b7d0397a49c1094316e2bf32f314
root <root@PC-CLARA.localdomain>
2021-07-07 19:56:14 +0200

    done

---
+++ Module_07/ex_02/Array.hpp
@@ -0,0 +1,69 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+
+template <typename T>
+class   Array
+{
+    private:
+        T   *_array;
+        int _n;
+
+    public:
+        Array() : _array(NULL), _n(0) {};
+        Array(unsigned int n) : _array(new T[n]), _n(n) {};
+        Array(Array  &a2) : _n(a2.size())
+        {
+            this->_array = new T[this->_n];
+            for (int i = 0; i < this->_n; i++)
+                this->_array[i] = a2[i];
+        };
+
+        virtual ~Array()
+        {
+            if (this->_array)
+                delete [] this->_array;
+        };
+
+        class OutOfLimits: public std::exception
+        {
+            public:
+				const char	*what() const throw(){
+                    return "Error: Out Of Limits Array";
+                }
+        };
+
+        Array &operator=(Array &a2)
+        {
+            this->_n = a2.size();
+            if (this->_array)
+                delete [] this->_array; //ver si va llamando al destruc;
+            this->_array = new T[this->_n];
+            for (int i = 0; i < this->_n; i++)
+                this->_array[i] = a2[i];
+            return *this;
+        };
+
+        T     &operator[](int i) const
+        {
+            try
+            {
+                if (i < 0 || i >= this->_n)
+                    throw OutOfLimits();
+            }
+            catch (std::exception &e)
+            {
+                std::cout << e.what() << std::endl;
+            }
+            return this->_array[i];
+        };
+        
+
+        int size()
+        {
+            return this->_n;
+        };
+};
+
+#endif
---
+++ Module_07/ex_02/main.cpp
@@ -0,0 +1,104 @@
+#include "Array.hpp"
+
+int main()
+{
+    Array<int> ia;
+    Array<int> ib(7);
+    Array<int> ic(ib);
+
+    Array<char> ca;
+    Array<char> cb(7);
+    Array<char> cc(cb);
+
+
+    std::cout << "------------------- " << std::endl;
+    std::cout << "--- INT ARRAYS --- " << std::endl;
+    std::cout << "------------------- " << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "--- EMPTY ARRAY --- " << std::endl;
+    try
+	{
+		std::cout << &ia << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+    std::cout << std::endl;
+
+    std::cout << "--- SIZE N ARRAY --- " << std::endl;
+    
+    for (int i = 0; i < 5; i++)
+        ib[i] = 2;
+
+	std::cout << ib[4] << std::endl;
+	std::cout << ib[10] << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "--- OVERLOAD OPERATOR= --- " << std::endl;
+
+    ia = ib;
+ 
+	std::cout << ia[4] << std::endl;
+	std::cout << ia[10] << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "------------------- " << std::endl;
+    std::cout << "--- CHAR ARRAYS --- " << std::endl;
+    std::cout << "------------------- " << std::endl;
+    std::cout << std::endl;
+    std::cout << "--- EMPTY ARRAY --- " << std::endl;
+
+    try
+	{
+		std::cout << &ca << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+    std::cout << std::endl;
+
+    std::cout << "--- SIZE N ARRAY --- " << std::endl;
+
+    for (int i = 0; i < 7; i++)
+		cb[i] = 97 + i;
+
+	for (int i = 0; i < 7; i++)
+		std::cout << cb[i] << " ";
+	std::cout << std::endl;
+
+	std::cout << cb[8] << " ";
+	std::cout << std::endl;
+
+    std::cout << "--- OVERLOAD OPERATOR= --- " << std::endl;
+
+    ca= cb;
+	std::cout << ca[4] << std::endl;
+	std::cout << ca[10] << std::endl;
+
+    cc = cb;
+    Array<char> cd(cc);
+	for (int i = 0; i < 7; i++)
+		std::cout << cd[i] << " ";
+	std::cout << std::endl;
+
+    std::cout << "------------------- " << std::endl;
+    std::cout << "--- OTHER ARRAYS --- " << std::endl;
+    std::cout << "------------------- " << std::endl;
+
+    Array<float> fa;
+    std::cout << &fa << std::endl;
+
+    Array<double> db(5);
+    for (int i = 0; i < 5; i++)
+        db[i] = i * 0.25;
+    for (int i = 0; i < 5; i++)
+        std::cout << db[i] << " ";
+    std::cout << std::endl;
+    
+    
+    return 0;
+
+}
